068bcc804d20052ad79e36de95198957a8ca879e
fdovasquez <51433004+fdovasquez@users.noreply.github.com>
2026-06-17 17:13:16 -0300

    Use profile password routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from django.views.generic import RedirectView
 
@@ -8,18 +9,8 @@ urlpatterns = [
     path("", RedirectView.as_view(pattern_name="device-list", permanent=False)),
     path("admin/", admin.site.urls),
     path("app/", include("inventory.urls")),
-    path(
-        "app/password/",
-        auth_views.PasswordChangeView.as_view(
-            template_name="inventory/password_change.html",
-            success_url="/app/password/done/",
-        ),
-        name="password-change",
-    ),
-    path(
-        "app/password/done/",
-        auth_views.PasswordChangeDoneView.as_view(template_name="inventory/password_change_done.html"),
-        name="password-change-done",
-    ),
     path("api/v1/metrics/", include("metrics.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
