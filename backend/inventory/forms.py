10fbb7442b4e0c59a069d353c917879fed6cd83b
fdovasquez <51433004+fdovasquez@users.noreply.github.com>
2026-06-17 03:47:50 -0300

    Agregar formularios de usuarios

---
+++ backend/inventory/forms.py
@@ -0,0 +1,79 @@
+from django import forms
+from django.contrib.auth.models import Group, User
+
+
+ROLE_NAMES = ["Administrador", "Editor", "Visualizador"]
+
+
+def ensure_base_roles():
+    for role_name in ROLE_NAMES:
+        Group.objects.get_or_create(name=role_name)
+
+
+def role_queryset():
+    ensure_base_roles()
+    return Group.objects.filter(name__in=ROLE_NAMES).order_by("name")
+
+
+class UserCreateForm(forms.ModelForm):
+    password = forms.CharField(label="Clave", widget=forms.PasswordInput)
+    role = forms.ModelChoiceField(label="Rol", queryset=Group.objects.none())
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "is_active"]
+        labels = {
+            "username": "Usuario",
+            "first_name": "Nombre",
+            "last_name": "Apellido",
+            "email": "Email",
+            "is_active": "Activo",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["role"].queryset = role_queryset()
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        user.is_staff = self.cleaned_data["role"].name in {"Administrador", "Editor"}
+        user.is_superuser = False
+        if commit:
+            user.save()
+            user.groups.set([self.cleaned_data["role"]])
+        return user
+
+
+class UserEditForm(forms.ModelForm):
+    password = forms.CharField(label="Nueva clave", widget=forms.PasswordInput, required=False)
+    role = forms.ModelChoiceField(label="Rol", queryset=Group.objects.none())
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "email", "is_active"]
+        labels = {
+            "username": "Usuario",
+            "first_name": "Nombre",
+            "last_name": "Apellido",
+            "email": "Email",
+            "is_active": "Activo",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["role"].queryset = role_queryset()
+        current_role = self.instance.groups.filter(name__in=ROLE_NAMES).first()
+        if current_role:
+            self.fields["role"].initial = current_role
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if self.cleaned_data.get("password"):
+            user.set_password(self.cleaned_data["password"])
+        user.is_staff = self.cleaned_data["role"].name in {"Administrador", "Editor"}
+        user.is_superuser = user.is_superuser and self.cleaned_data["role"].name == "Administrador"
+        if commit:
+            user.save()
+            user.groups.set([self.cleaned_data["role"]])
+        return user
