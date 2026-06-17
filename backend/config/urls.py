708bb7d199e8d955317945e8c9488b6fe4bc3fc1
fdovasquez <51433004+fdovasquez@users.noreply.github.com>
2026-06-17 02:04:50 -0300

    Agregar rutas de aplicacion web

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="device-list", permanent=False)),
     path("admin/", admin.site.urls),
+    path("app/", include("inventory.urls")),
     path("api/v1/metrics/", include("metrics.urls")),
 ]
