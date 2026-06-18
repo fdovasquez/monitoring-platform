1518ef601a264d55642601a84266cc39ac7df037
fdovasquez <51433004+fdovasquez@users.noreply.github.com>
2026-06-18 02:07:44 -0300

    Accept inventory data in metric ingest

--- backend/metrics/serializers.py
+++ backend/metrics/serializers.py
@@ -6,4 +6,5 @@ class MetricIngestSerializer(serializers.Serializer):
     agent_version = serializers.CharField(max_length=50, required=False, allow_blank=True)
     timestamp = serializers.DateTimeField()
     metrics = serializers.DictField()
+    inventory = serializers.DictField(required=False)
     services = serializers.ListField(required=False)
